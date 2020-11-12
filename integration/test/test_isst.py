b18c4179890fd58477dc2e6b01f9d2cab6f82841
Asma H Al-Rawi <asma.h.al-rawi@intel.com>
2020-11-12 16:04:40 -0500

    First commit SST integration tests for OS MBox registers.

---
+++ integration/test/test_isst.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+#
+#  Copyright (c) 2015, 2016, 2017, 2018, 2019, 2020, Intel Corporation
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in
+#        the documentation and/or other materials provided with the
+#        distribution.
+#
+#      * Neither the name of Intel Corporation nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+from __future__ import absolute_import
+
+import os
+import sys
+import unittest
+import shlex
+import subprocess
+import io
+import json
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import geopm_context
+import geopmpy.io
+import util
+import geopmpy.topo
+import geopmpy.pio
+import geopm_test_launcher
+
+# Check running as root
+
+class TestIntegrationISST(unittest.TestCase):
+
+    def test_read_iss_config(self):
+        sst_comm=["intel-speed-select", "-f", "json", "perf-profile", "get-config-levels"]
+        sst_comm_ret=subprocess.run(sst_comm, capture_output=True)
+        sst_comm_json=json.loads(sst_comm_ret.stderr)
+
+        num_pkg = geopmpy.topo.num_domain('package')
+        num_core = geopmpy.topo.num_domain('core')
+
+        for pkg in range(num_pkg):
+            core_idx=int(pkg*(num_core/num_pkg))
+            sst_config_level=sst_comm_json["package-" + str(pkg) + ":die-0:cpu-" 
+                             + str(core_idx)]["get-config-levels"]
+
+            geopm_config_level = geopmpy.pio.read_signal("SST::CONFIG_LEVEL", "package", pkg)
+            self.assertEqual(int(sst_config_level), int(geopm_config_level))
+
+    def test_read_turbofreq_support(self):
+        sst_comm=["intel-speed-select", "--info"]
+        sst_comm_ret=subprocess.run(sst_comm, capture_output=True)
+        #TODO: Figure out how to parse output
+
+        geopm_tf_support = geopmpy.pio.read_signal("SST::TURBOFREQ_SUPPORT", "package", 0)
+        self.assertEqual(sst_comm_ret.stderr, geopm_tf_support)
+
+    def test_read_cp_support(self):
+        sst_comm=["intel-speed-select", "-f", "json", "core-power", "info"]
+        sst_comm_ret=subprocess.run(sst_comm, capture_output=True)
+        sst_comm_json=json.loads(sst_comm_ret.stderr)
+
+        num_pkg = geopmpy.topo.num_domain('package')
+        num_core = geopmpy.topo.num_domain('core')
+
+        for pkg in range(num_pkg):
+            core_idx=int(pkg*(num_core/num_pkg))
+            sst_cp_support=sst_comm_json["package-" + str(pkg) + ":die-0:cpu-" 
+                             + str(core_idx)]["core-power"]["support-status"]
+            if sst_cp_support == "supported":
+                sst_cp_support=1
+            else:
+                sst_cp_support=0
+
+            geopm_cp_support = geopmpy.pio.read_signal("SST::COREPRIORITY_SUPPORT","package", pkg)
+            self.assertEqual(int(sst_cp_support), int(geopm_cp_support))
+
+    def test_read_turbofreq_status(self):
+        num_pkg = geopmpy.topo.num_domain('package')
+        for pkg in range(num_pkg):
+            # Check when disabled
+            sst_comm=["intel-speed-select", "-f", "json", "turbo-freq", "disable", "-l", str(pkg)]
+            sst_comm_ret=subprocess.run(sst_comm, capture_output=True)
+
+            self.assertEqual(0, sst_comm_ret.returncode)
+
+            turbofreq_status = geopmpy.pio.read_signal("SST::TURBOFREQ_STATUS", "package", pkg)
+            self.assertEqual(0, turbofreq_status)
+
+            # Check when enabled
+            sst_comm=["intel-speed-select", "-f", "json", "turbo-freq", "enable", "-l", str(pkg)]
+            sst_comm_ret=subprocess.run(sst_comm, capture_output=True)
+
+            self.assertEqual(0, sst_comm_ret.returncode)
+
+            turbofreq_status = geopmpy.pio.read_signal("SST::TURBOFREQ_STATUS", "package", pkg)
+            self.assertEqual(1, turbofreq_status)
+
+    def test_read_cp_status(self):
+        sst_comm=["intel-speed-select", "-f", "json", "core-power", "info"]
+        sst_comm_ret=subprocess.run(sst_comm, capture_output=True)
+        sst_comm_json=json.loads(sst_comm_ret.stderr)
+
+        num_pkg = geopmpy.topo.num_domain('package')
+        num_core = geopmpy.topo.num_domain('core')
+
+        for pkg in range(num_pkg):
+            core_idx=int(pkg*(num_core/num_pkg))
+            sst_cp_status=sst_comm_json["package-" + str(pkg) + ":die-0:cpu-" 
+                             + str(core_idx)]["core-power"]["enable-status"]
+            if sst_cp_status == "statused":
+                sst_cp_status=1
+            else:
+                sst_cp_status=0
+
+            geopm_cp_status = geopmpy.pio.read_signal("SST::COREPRIORITY_SUPPORT","package", pkg)
+            self.assertEqual(int(sst_cp_status), int(geopm_cp_status))
+
+    def test_read_hp_ncores(self):
+        sst_comm=["intel-speed-select", "-f", "json", "turbo-freq", "info", "-l", "0"]
+        sst_comm_ret=subprocess.run(sst_comm, capture_output=True)
+        sst_comm_json=json.loads(sst_comm_ret.stderr)
+
+        num_pkg = geopmpy.topo.num_domain('package')
+        num_core = geopmpy.topo.num_domain('core')
+
+        for pkg in range(num_pkg):
+            core_idx=int(pkg*(num_core/num_pkg))
+            #TODO: Get dynamically # buckets supported
+            for bucket_idx in range(3):
+                sst_ncore_bucket=sst_comm_json["package-" + str(pkg) + ":die-0:cpu-" 
+                             + str(core_idx)]["speed-select-turbo-freq-properties"]["bucket-"
+                             + str(bucket_idx)]["high-priority-cores-count"]
+                geopm_ncore_bucket = geopmpy.pio.read_signal("SST::HIGHPRIORITY_NCORES_"
+                             + str(bucket_idx), "package", pkg)
+
+                self.assertEqual(int(sst_ncore_bucket), int(geopm_ncore_bucket))
+
+    def test_read_hp_trl(self):
+        sst_comm=["intel-speed-select", "-f", "json", "turbo-freq", "info", "-l", "0"]
+        sst_comm_ret=subprocess.run(sst_comm, capture_output=True)
+        sst_comm_json=json.loads(sst_comm_ret.stderr)
+
+        num_pkg = geopmpy.topo.num_domain('package')
+        num_core = geopmpy.topo.num_domain('core')
+
+        for pkg in range(num_pkg):
+            core_idx=int(pkg*(num_core/num_pkg))
+            #TODO: Get dynamically # buckets supported
+            for bucket_idx in range(3):
+                sst_sse_trl=1e6*int(sst_comm_json["package-" + str(pkg) + ":die-0:cpu-" 
+                             + str(core_idx)]["speed-select-turbo-freq-properties"]["bucket-"
+                             + str(bucket_idx)]["high-priority-max-frequency(MHz)"])
+                sst_avx_trl=1e6*int(sst_comm_json["package-" + str(pkg) + ":die-0:cpu-" 
+                             + str(core_idx)]["speed-select-turbo-freq-properties"]["bucket-"
+                             + str(bucket_idx)]["high-priority-max-avx2-frequency(MHz)"])
+
+                sst_avx3_trl=1e6*int(sst_comm_json["package-" + str(pkg) + ":die-0:cpu-" 
+                             + str(core_idx)]["speed-select-turbo-freq-properties"]["bucket-"
+                             + str(bucket_idx)]["high-priority-max-avx512-frequency(MHz)"])
+
+                geopm_sse_trl = geopmpy.pio.read_signal("SST::HIGHPRIORITY_FREQUENCY_SSE_"
+                             + str(bucket_idx), "package", pkg)
+                geopm_avx_trl = geopmpy.pio.read_signal("SST::HIGHPRIORITY_FREQUENCY_AVX2_"
+                             + str(bucket_idx), "package", pkg)
+                geopm_avx3_trl = geopmpy.pio.read_signal("SST::HIGHPRIORITY_FREQUENCY_AVX512_"
+                             + str(bucket_idx), "package", pkg)
+
+                self.assertEqual(int(sst_sse_trl), int(geopm_sse_trl))
+                self.assertEqual(int(sst_avx_trl), int(geopm_avx_trl))
+                self.assertEqual(int(sst_avx2_trl), int(geopm_avx2_trl))
+
+    def test_read_lp_trl(self):
+        sst_comm=["intel-speed-select", "-f", "json", "turbo-freq", "info", "-l", "0"]
+        sst_comm_ret=subprocess.run(sst_comm, capture_output=True)
+        sst_comm_json=json.loads(sst_comm_ret.stderr)
+
+        num_pkg = geopmpy.topo.num_domain('package')
+        num_core = geopmpy.topo.num_domain('core')
+
+        for pkg in range(num_pkg):
+            core_idx=int(pkg*(num_core/num_pkg))
+
+            lp_trl=sst_comm_json["package-" + str(pkg) + ":die-0:cpu-" + str(core_idx)]["speed-select-turbo-freq-properties"]["speed-select-turbo-freq-clip-frequencies"]
+            sst_sse_trl=1e6*int(lp_trl["low-priority-max-frequency(MHz)"])
+            sst_avx2_trl=1e6*int(lp_trl["low-priority-max-avx2-frequency(MHz)"])
+            sst_avx3_trl=1e6*int(lp_trl["low-priority-max-avx512-frequency(MHz)"])
+            geopm_sse_trl = geopmpy.pio.read_signal("SST::LOWPRIORITY_FREQUENCY_SSE", "package", pkg)
+            geopm_avx_trl = geopmpy.pio.read_signal("SST::LOWPRIORITY_FREQUENCY_AVX2", "package", pkg)
+            geopm_avx3_trl = geopmpy.pio.read_signal("SST::LOWPRIORITY_FREQUENCY_AVX512", "package", pkg)
+                
+            self.assertEqual(int(sst_sse_trl), int(geopm_sse_trl))
+            self.assertEqual(int(sst_avx_trl), int(geopm_avx_trl))
+            self.assertEqual(int(sst_avx2_trl), int(geopm_avx2_trl))
+
+if __name__ == '__main__':
+    unittest.main()
