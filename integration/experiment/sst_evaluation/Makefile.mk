a2b805cdbc987f336e4ff213782811fb2ef9bf87
Daniel Wilson <daniel1.wilson@intel.com>
2022-07-12 07:30:19 -0700

    Add the sst_evaluation experiment
    - Add NAS-BT and AIB experiments
    - Add a plot generator

---
+++ integration/experiment/sst_evaluation/Makefile.mk
@@ -0,0 +1,10 @@
+#  Copyright (c) 2015 - 2022, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+EXTRA_DIST += \
+              integration/experiment/sst_evaluation/__init__.py \
+              integration/experiment/sst_evaluation/sst_evaluation.py \
+              integration/experiment/sst_evaluation/README.md \
+              integration/experiment/sst_evaluation/run_sst_evaluation_arithmetic_intensity.py \
+              # end
