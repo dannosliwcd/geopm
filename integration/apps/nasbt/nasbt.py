8b0a0a65f0aa70d81f630d4cb8b1ef1aaa0ec618
Daniel Wilson <daniel1.wilson@intel.com>
2022-08-30 12:23:34 -0700

    Remove impi specifics from nasbt.py

--- integration/apps/nasbt/nasbt.py
+++ integration/apps/nasbt/nasbt.py
@@ -2,11 +2,13 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 #
 
+
 '''
 AppConf class for NAS BT.
 '''
 
 import os
+import math
 
 from apps import apps
 from math import sqrt
@@ -15,34 +17,39 @@ from math import sqrt
 def setup_run_args(parser):
     """ Add common arguments for all run scripts.
     """
-    parser.add_argument('--npb-class', default='C')
+    parser.add_argument('--npb-class', default='D')
     parser.add_argument('--ranks-per-node', dest='ranks_per_node',
                         action='store', type=int,
                         help='Number of physical cores to reserve for the app. '
-                             'If not defined, all nodes but one will be reserved (leaving one '
-                             'node for GEOPM).')
+                             'If not defined, all nodes but one will be reserved, '
+                             'rounding down to a square number and leaving one '
+                             'core for GEOPM.')
 
 def create_appconf(mach, args):
-    ''' Create a ArithmeticIntensityAppConf object from an ArgParse and experiment.machine object.
+    ''' Create a NASBTAppConf object from an ArgParse and experiment.machine object.
     '''
-    return NASBTAppConf(mach, args.npb_class, args.ranks_per_node)
+    return NASBTAppConf(mach, args.npb_class, args.ranks_per_node, args.node_count)
 
 class NASBTAppConf(apps.AppConf):
     @staticmethod
     def name():
         return 'nas_bt'
 
-    def __init__(self, mach, npb_class, ranks_per_node):
+    def __init__(self, mach, npb_class, ranks_per_node, node_count):
         benchmark_dir = os.path.dirname(os.path.abspath(__file__))
-        self._exec_path = os.path.join(benchmark_dir, "NPB3.4.1-MZ", "NPB3.4-MZ-MPI", "bin", "bt-mz." + npb_class + ".x")
+        self._exec_path = os.path.join(benchmark_dir, "NPB3.4.2", "NPB3.4-MPI", "bin", "bt." + npb_class + ".x")
         self._exec_args = []
         if ranks_per_node is None:
-            self._ranks_per_node = mach.num_core() - 1
+            # Leave one core for non-app work (e.g., geopm)
+            total_ranks = (mach.num_core() - 1) * node_count
         else:
             if ranks_per_node > mach.num_core():
-                raise RuntimeError('Number of requested cores is more than the number of available ' +
-                                   'cores: {} vs. {}'.format(ranks_per_node, mach.num_core()))
-            self._ranks_per_node = ranks_per_node
+                raise RuntimeError('The count of requested cores ({}) is more than the number of available cores ({})'.format(ranks_per_node, mach.num_core()))
+            total_ranks = ranks_per_node * node_count
+
+        # The count of NPB BT processes must be a square number
+        total_ranks = math.floor(sqrt(total_ranks)) ** 2
+        self._ranks_per_node = math.ceil(total_ranks / node_count)
 
     def get_rank_per_node(self):
         return self._ranks_per_node
@@ -50,13 +57,12 @@ class NASBTAppConf(apps.AppConf):
     def get_cpu_per_rank(self):
         return 1
 
-    def get_custom_geopm_args(self):
-        args = ['--geopm-ompt-disable',
-                '--geopm-affinity-disable',
-                '-genv', 'I_MPI_PIN_DOMAIN=auto',
-                '-genv', 'I_MPI_PIN_ORDER=scatter',
-                ]
-        return args
+    def get_bash_setup_commands(self):
+        # The experiment launcher assumes ranks_per_node is equal across all nodes.
+        # In reality, some may have one less rank than others. This means
+        # ranks_per_node * node_count does not equal total_ranks. Tell NPB
+        # to go ahead and round down the total rank count when this happens.
+        return 'export NPB_NPROCS_STRICT=0\n'
 
     def parse_fom(self, log_path):
         with open(log_path) as fid:
