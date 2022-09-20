3fe35098aa4d7266efdda0a7f61ff7cf0c7a1038
Daniel Wilson <daniel1.wilson@intel.com>
2022-09-20 06:44:33 -0700

    Add NPB MG appconf

---
+++ integration/experiment/monitor/run_monitor_nasmg.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#
+#  Copyright (c) 2015 - 2022, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+'''
+Run nasmg with the monitor agent.
+'''
+
+import argparse
+
+from experiment.monitor import monitor
+from experiment import machine
+from apps.nasmg import nasmg
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    monitor.setup_run_args(parser)
+    nasmg.setup_run_args(parser)
+    args, extra_args = parser.parse_known_args()
+    mach = machine.init_output_dir(args.output_dir)
+    app_conf = nasmg.create_appconf(mach, args)
+    monitor.launch(app_conf=app_conf, args=args,
+                   experiment_cli_args=extra_args)
