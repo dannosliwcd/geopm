cc8653061649cf8e2fb7861afeae34f261cf400d
Daniel Wilson <daniel1.wilson@intel.com>
2022-09-26 08:19:08 -0700

    Add NPB SP-MZ appconf

---
+++ integration/experiment/monitor/run_monitor_nasspmz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#
+#  Copyright (c) 2015 - 2022, Intel Corporation
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+
+'''
+Run NPB SP-MZ with the monitor agent.
+'''
+
+import argparse
+
+from experiment.monitor import monitor
+from experiment import machine
+from apps.nassp import nassp
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    monitor.setup_run_args(parser)
+    nassp.setup_run_args(parser)
+    args, extra_args = parser.parse_known_args()
+    mach = machine.init_output_dir(args.output_dir)
+    app_conf = nassp.create_appconf(mach, args)
+    monitor.launch(app_conf=app_conf, args=args,
+                   experiment_cli_args=extra_args)
