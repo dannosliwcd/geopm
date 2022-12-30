0f16d6f563123ce510107d3d1c0caaf0a4f3c48b
Daniel Wilson <daniel1.wilson@intel.com>
2022-12-30 07:32:19 -0800

    Emit a run-time warning if io_uring is requested but cannot be used

--- service/src/IOUring.cpp
+++ service/src/IOUring.cpp
@@ -6,17 +6,35 @@
 
 #include "IOUring.hpp"
 
+#include <iostream>
+
 #include "IOUringFallback.hpp"
 #include "IOUringImp.hpp"
 
 namespace geopm
 {
+#ifdef GEOPM_HAS_IO_URING
+    static void emit_missing_support_warning()
+    {
+#ifdef GEOPM_DEBUG
+        static bool do_emit_warning = true;
+        if (do_emit_warning) {
+            do_emit_warning = false;
+            std::cerr << "Warning: <geopm> GEOPM was built with liburing "
+                "enabled, but the system does not support all uring operations "
+                "needed by GEOPM. Using non-uring IO instead." << std::endl;
+        }
+#endif
+    }
+#endif
+
     std::shared_ptr<IOUring> make_io_uring(unsigned entries)
     {
 #ifdef GEOPM_HAS_IO_URING
         if (IOUringImp::is_supported()) {
             return make_io_uring_imp(entries);
         }
+        emit_missing_support_warning();
 #endif
         return make_io_uring_fallback(entries);
     }
