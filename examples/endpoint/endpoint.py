c45f9c0f0cb39d36915effbde6c9f875dc734e36
Daniel Wilson <daniel1.wilson@intel.com>
2023-01-31 16:13:30 -0800

    Make the endpoint example launch a workload and report power summaries

--- examples/endpoint/endpoint.py
+++ examples/endpoint/endpoint.py
@@ -2,21 +2,45 @@
 
 import geopmpy.endpoint
 import geopmpy.agent
+import subprocess
 import time
 import os
+import pandas as pd
+
+from yaml import load
+try:
+    from yaml import CSafeLoader as SafeLoader
+except ImportError:
+    from yaml import SafeLoader
+
+NNODES = int(os.environ['SLURM_NNODES'])
+HOME = os.environ['HOME']
+
+ENDPOINT_PATH = '/dev/shm/testendpoint'
+# TODO: must this reside on a shared file system? Or is local okay if we get the right host?
+ENDPOINT_PATH = os.path.join(HOME, 'testendpoint')
+
+REPORT_PATH = 'geopm.report'
+TRACE_PATH = 'geopm.trace'
+BENCH_CONF_PATH = os.path.join(HOME, 'bench.conf')
 
 try:
-    os.remove('testendpoint-policy')
-    os.remove('testendpoint-sample')
+    os.remove(f'{ENDPOINT_PATH}-policy')
+    os.remove(f'{ENDPOINT_PATH}-sample')
 except FileNotFoundError:
     pass
 # TODO: CWD needs to be same on both endpoint sides
-endpoint = geopmpy.endpoint.Endpoint('testendpoint')
+endpoint = geopmpy.endpoint.Endpoint(ENDPOINT_PATH)
 endpoint.open()
+
+proc = subprocess.Popen(['geopmlaunch', 'srun', '-N', f'{NNODES}', '-n', f'{NNODES*2}',
+                         f'--geopm-endpoint={ENDPOINT_PATH}',
+                         f'--geopm-report={REPORT_PATH}', f'--geopm-trace={TRACE_PATH}',
+                         '--geopm-trace-signals=CPU_POWER_LIMIT_CONTROL@board',
+                         '--geopm-agent=power_governor', 'geopmbench', BENCH_CONF_PATH])
+
 print('Waiting for an agent to attach...')
 endpoint.wait_for_agent_attach(timeout=10)
-
-# geopmlaunch srun -N 1 -n 2 --geopm-endpoint=testendpoint --geopm-trace=geopm.trace --geopm-trace-signals=CPU_POWER_LIMIT_CONTROL@board --geopm-agent=power_governor geopmbench  ~/bench.conf
 
 agent_name = endpoint.agent()
 policy_names = geopmpy.agent.policy_names(agent_name)
@@ -26,7 +50,7 @@ print('Policy names:', policy_names)
 print('Sample names:', geopmpy.agent.sample_names(agent_name))
 
 endpoint.write_policy({'CPU_POWER_LIMIT': 150})
-time.sleep(1)
+time.sleep(2)
 
 if 'CPU_POWER_LIMIT' in policy_names:
     for i in range(8):
@@ -37,6 +61,19 @@ if 'CPU_POWER_LIMIT' in policy_names:
             endpoint.write_policy({'CPU_POWER_LIMIT': limit})
             print('Set limit:', limit)
         except RuntimeError:
-            print('Encountered an error. Exiting.')
             break
         time.sleep(2)
+
+proc.wait()
+
+with open(REPORT_PATH) as f:
+    report = load(f, Loader=SafeLoader)
+
+for host, host_data in report['Hosts'].items():
+    print(f'===== Host {host} time: {host_data["Application Totals"]["runtime (s)"]} =====')
+    df = pd.read_csv(f'{TRACE_PATH}-{host}', sep='|', comment='#', na_values=['nan', 'NAN'], index_col='TIME')
+    budget_changes = df.loc[df['POWER_BUDGET'].shift(1) != df['POWER_BUDGET'], ['CPU_ENERGY', 'POWER_BUDGET']].copy()
+    budget_changes['MEAN_POWER'] = budget_changes['CPU_ENERGY'].diff() / budget_changes.index.to_series().diff()
+
+    print(budget_changes.to_string())
+
