faaffe7228a341a3a760aab4f95eae3fc1f25a84
Daniel Wilson <daniel1.wilson@intel.com>
2023-03-16 08:38:16 -0700

    Make bench launcher use the power_governor

--- examples/endpoint/launch_geopmbench_endpoint.sh
+++ examples/endpoint/launch_geopmbench_endpoint.sh
@@ -7,11 +7,11 @@ fi
 CONF_PATH="$1"
 
 source ~/geopm/integration/config/run_env.sh
-#python3 ./balance_client.py \
+python3 ./balance_client.py \
         geopmlaunch srun -N "$SLURM_NNODES" -n $((SLURM_NNODES * 2)) \
         --geopm-report="job.${SLURM_JOBID}.report" \
         --geopm-trace="job.${SLURM_JOBID}.trace" \
-        --geopm-agent=monitor \
+        --geopm-agent=power_governor \
         --geopm-trace-signals=CPU_POWER_LIMIT_CONTROL \
         -- \
         geopmbench "${CONF_PATH}"
