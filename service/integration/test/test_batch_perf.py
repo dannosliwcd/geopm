266af0f2a20b6baed580f0b43374957273858254
Daniel Wilson <daniel1.wilson@intel.com>
2024-02-02 11:50:25 -0800

    Plot geopm batch read times by privilege level, msr backend, and first/rest iterations

--- service/integration/test/test_batch_perf.py
+++ service/integration/test/test_batch_perf.py
@@ -1,27 +1,48 @@
 #!/usr/bin/env python3
-#  Copyright (c) 2015 - 2024, Intel Corporation
-#  SPDX-License-Identifier: BSD-3-Clause
-#
 
-import matplotlib.pyplot as plt
+import argparse
+import seaborn as sns
+import pandas as pd
+import matplotlib.ticker as mtick
+import os
 
-def main():
-    cases = ['msr-safe-root',
-             'msr-safe-service',
-             'msr-sync-root',
-             'msr-sync-service',
-             'msr-uring-root',
-             'msr-uring-service']
-    time_samples = dict()
-    for cc in cases:
-        time_samples[cc] = []
-        with open(f'{cc}.csv') as fid:
-            fid.readline()
-            for line in fid.readlines():
-                (count, time) = line.split(',')
-                time_samples[cc].append(float(time))
+parser = argparse.ArgumentParser()
+parser.add_argument('csvs', nargs='+',
+                    help='One or more CSV files containing DURATION of each batch operation.')
+parser.add_argument('--plot-path', default='./geopm_batch_read_latency.png',
+                    help='Where to save the generated plot.')
+parser.add_argument('--min-y', type=float, default=0.0001,
+                    help='Minimum value to show on the y axis, in seconds.')
 
-    height = [sum(vv)/len(vv) for vv in time_samples.values()]
-    plt.bar(x=range(len(time_samples)), height=height, tick_label=time_samples.keys())
-if __name__ == '__main__':
-    main()
+args = parser.parse_args()
+
+sns.set()
+
+configs = [os.path.splitext(os.path.basename(csv_path))[0] for csv_path in args.csvs]
+df = pd.concat([pd.read_csv(csv_path) for csv_path in args.csvs], keys=configs, names=['Config', 'sample'])
+df[['Backend', 'Privilege']] = (df.index.get_level_values('Config')
+                                .to_series(index=df.index).str.rsplit('-', 1, expand=True))
+df['Which Iteration'] = 'First'
+df.loc[(slice(None), slice(1, None)), 'Which Iteration'] = 'Rest'
+df['Latency (ms)'] = df['DURATION'] * 1000
+g = sns.catplot(
+    data=df.reset_index(),
+    kind='bar',
+    col='Backend',
+    x='Privilege',
+    aspect=0.5,
+    hue='Which Iteration',
+    ci='sd',
+    y='DURATION',
+    log=True,
+    facet_kws=dict(sharey=False),
+)
+g.set(ylim=(args.min_y, None), xlabel=None)
+g.set_axis_labels(y_var='Latency')
+for ax in g.axes.flat:
+    ax.grid(True, which="both", axis='y')
+    locmin = mtick.LogLocator(base=10, numticks=10)
+    ax.xaxis.set_minor_locator(locmin)
+    ax.yaxis.set_major_formatter(mtick.EngFormatter('s'))
+g.tight_layout()
+g.savefig(args.plot_path, bbox_inches='tight')
